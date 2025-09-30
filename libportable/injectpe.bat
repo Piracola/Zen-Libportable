2cb5535391431d0c5ee2df9d38ab16e65b1056c3
Piracola <116626041+Piracola@users.noreply.github.com>
2025-10-01 00:46:10 +0800

    Update libportable version

--- libportable/injectpe.bat
+++ libportable/injectpe.bat
@@ -1,46 +1,48 @@
 @echo off
 if exist "%~dp0zen.exe" (echo.) && if exist "%~dp0mozglue.dll" (
-echo 已找到需要注入的文件zen.exe. 
+echo ���ҵ���Ҫע����ļ�zen.exe. 
 goto platform
 ) else (
-echo 没找到相关文件,请把程序放入浏览器的安装目录执行.
+echo û�ҵ�����ļ�,��ѳ������������İ�װĿ¼ִ��.
 goto eof
 )
 
 :platform
 if exist "%~dp0upcheck.exe" del /s/q "%~dp0upcheck.exe" 2>nul 1>nul
-if "%PROCESSOR_ARCHITECTURE%"=="x86" setdll32 /t:zen.exe 2>nul
-if "%PROCESSOR_ARCHITECTURE%"=="AMD64" setdll64 /t:zen.exe 2>nul
+if exist "%~dp0upcheck32.exe" (
+    upcheck32.exe -file 2>nul
+) else (
+    if exist "%~dp0upcheck64.exe" upcheck64.exe -file 2>nul
+)
 if "%errorlevel%"=="32" set bits=32&goto x86
 if "%errorlevel%"=="64" set bits=64&goto x64
 goto eof
 
 :x86
 @echo ***********************************************************************
-@echo *                程序自动注入32位的zen浏览器                      *
-@echo *注意!!! 新版本会修复omni.ja文件,不要在同一zen版本上重复运行此脚本*
+@echo *                �����Զ�ע��32λ��zen�����                      *
 @echo ***********************************************************************
 echo+
+@pause .
 if exist "%~dp0upcheck32.exe" rename upcheck32.exe upcheck.exe 2>nul
 goto runing
 
 :x64
 echo+
 @echo ***********************************************************************
-@echo *                程序自动注入64位的zen浏览器                      *
-@echo *注意!!! 新版本会修复omni.ja文件,不要在同一zen版本上重复运行此脚本*
+@echo *                �����Զ�ע��64λ��zen�����                      *
 @echo ***********************************************************************
 echo+
-@pause.
+@pause .
 if exist "%~dp0upcheck64.exe" rename upcheck64.exe upcheck.exe 2>nul
 
 :runing
-setdll%bits% /d:portable%bits%.dll mozglue.dll 2>nul
-if exist "%~dp0upcheck.exe" setdll%bits% /p:browser\omni.ja 2>nul
-if "%errorlevel%"=="0" echo 注入成功,请仔细阅读readme.txt和portable(example).ini文件!&goto eof
-echo 程序注入失败,请联系开发者!
+if exist "%~dp0upcheck.exe" upcheck.exe -dll 2>nul
+if "%errorlevel%"=="0" echo ע��ɹ�,����ϸ�Ķ�README��portable(example).ini�ļ�!&goto eof
+echo ����ע��ʧ��, ��ο�������Ϣ, ������֮ǰע���.
 
 :eof
+pause .
 @del /s/q setdll*.exe 2>nul 1>nul
 if "%bits%"=="32" del /s/q portable64.dll upcheck64.exe 2>nul 1>nul
 if "%bits%"=="64" del /s/q portable32.dll upcheck32.exe 2>nul 1>nul
